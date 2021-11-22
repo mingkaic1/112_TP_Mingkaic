790aff7bcb01eb49a52537d100099704936efffd
mingkaic1 <mingkaic@andrew.cmu.edu>
2021-11-22 01:04:08 -0500

    Working Game AI player tracking

--- gameAI.py
+++ gameAI.py
@@ -10,6 +10,8 @@ class GameAI():
         self.path = None
         self.getNewPath()
 
+        self.isMoving = False
+
     def update(self):
         if self.path != None:
             # If .targetTank is not yet within line-of-sight
@@ -34,9 +36,78 @@ class GameAI():
             return True
         return False
 
-
     def moveTowardTargetTank(self):
-        print(self.tank.theta)
+        self.steer()
+        self.move()
+
+    def steer(self):
+        angleToNextNode = self.getAngleToNextNode()
+        # If AI is currently not facing next node
+        if self.constrainAngle(self.tank.theta) != angleToNextNode:
+            self.rotateToAngle(angleToNextNode)
+        # If AI is facing next node
+        else:
+            self.stopRotation()
+
+    def rotateToAngle(self, targetAngle):
+        # Find the difference in angle to turn through (ensure within -180 to 180)
+        dAngle = targetAngle - self.constrainAngle(self.tank.theta)
+        while dAngle <= -180:
+            dAngle += 360
+        while dAngle > 180:
+            dAngle -= 360
+        print(dAngle)
+        # If faster to turn counterclockwise
+        if (dAngle > 0):
+            self.tank.startSteeringLeft()
+            self.tank.stopSteeringRight()
+        # If faster to turn clockwise
+        elif (dAngle < 0):
+            self.tank.startSteeringRight()
+            self.tank.stopSteeringLeft()
+        # If targetAngle is already reached
+        else:
+            self.stopRotation()
+
+    # Constrain an angle to 0-359
+    def constrainAngle(self, angle):
+        while angle < 0:
+            angle += 360
+        while angle >= 360:
+            angle -= 360
+        return angle
+
+    def stopRotation(self):
+        self.tank.stopSteeringLeft()
+        self.tank.stopSteeringRight()
+
+    # HELPER FUNCTION
+    # Get the angle (0, 90, 180, 270 deg) that the next node is in relation to current node
+    def getAngleToNextNode(self):
+        currentNode = self.path[0]
+        nextNode = self.path[1]
+        # If nextNode is in same row
+        if nextNode[0] == currentNode[0]:
+            # If nextNode is to the left
+            if nextNode[1] < currentNode[1]:
+                angleToNextNode = 90
+            # If nextNode is to the right
+            else:
+                angleToNextNode = 270
+        # If nextNode is in same col
+        else:
+            # If nextNode is above
+            if nextNode[0] < currentNode[0]:
+                angleToNextNode = 0
+            # If nextNode is below
+            else:
+                angleToNextNode = 180
+        return angleToNextNode
+
+    def move(self):
+        angleToNextNode = self.getAngleToNextNode()
+        if (self.constrainAngle(self.tank.theta) == angleToNextNode):
+            self.tank.startMovingForward()
 
     def shootAtTargetTank(self):
         pass
