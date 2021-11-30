69d9f90e1a2abd201a33791ba9c5804760b7990b
mingkaic1 <mingkaic@andrew.cmu.edu>
2021-11-30 13:34:03 -0500

    Added comments to settings.py

--- drawingHelpers.py
+++ drawingHelpers.py
@@ -4,12 +4,10 @@
 # --------------------
 
 def drawWalls(app, canvas):
-
     for wall in app.game.round.map.translatedWallRectangles:
         canvas.create_rectangle(*wall, fill="black")
 
 def drawTanks(app, canvas):
-
     # Get list of lists, each containing 4 tuples representing corners of tank polygon
     tanksTranslatedCorners = app.game.round.getTanksTranslatedCorners()
     for i in range(len(tanksTranslatedCorners)):
@@ -18,18 +16,15 @@ def drawTanks(app, canvas):
             canvas.create_polygon(tanksTranslatedCorners[i], fill=color)
 
 def drawProjectiles(app, canvas):
-
     # Get list of tuples, each representing 4 canvas coordinates of circle
     projectilesTranslatedCoordinates = app.game.round.getProjectilesTranslatedCoordinates()
     for i in range(len(projectilesTranslatedCoordinates)):
         canvas.create_oval(projectilesTranslatedCoordinates[i], fill="black")
 
 def drawFPS(app, canvas):
-
     canvas.create_text(5, 5, text=f"{round(app.fpsMeter.getFPS())}", anchor="nw")
 
 def drawScores(app, canvas):
-
     xGap = app.game.settings["WINDOW_WIDTH"] // (app.game.settings["NUM_PLAYERS"] + app.game.settings["NUM_AI"] + 1)
     x0 = (app.game.settings["WINDOW_WIDTH"] - (app.game.settings["NUM_PLAYERS"] + app.game.settings["NUM_AI"] - 1) * xGap) // 2
     y = ((app.game.settings["MARGIN"] + app.game.settings["NUM_ROWS"] * app.game.settings["MAPCELL_SIZE"]) + app.game.settings["WINDOW_HEIGHT"]) // 2
--- settings.py
+++ settings.py
@@ -5,9 +5,26 @@
 # --------------------
 
 settings = {
-    "NUM_PLAYERS": 2,
-    "NUM_AI": 2,
+
+    # Numbers of players and GameAIs
+    #   - Any combination "works," but higher numbers become buggier
+    #   - A Tank's controls must be defined below in "DEFAULT_CONTROLS" to be
+    #     player-controlled (otherwise, it just sits there)
+    #   - The Round will instantly end if there is only 1 Tank
+    "NUM_PLAYERS": 3,
+    "NUM_AI": 0,
+
+    # Define keyboard controls here
+    #   - If a non-AI Tank has no controls defined, it won't move, but the game
+    #     wouldn't crash
     "DEFAULT_CONTROLS": {
+        0: {
+            "forward": "w",
+            "backward": "s",
+            "left": "a",
+            "right": "d",
+            "fire": "Tab"
+        },
         1: {
             "forward": "Up",
             "backward": "Down",
@@ -15,14 +32,11 @@ settings = {
             "right": "Right",
             "fire": "Space"
         },
-        0: {
-            "forward": "w",
-            "backward": "s",
-            "left": "a",
-            "right": "d",
-            "fire": "Tab"
-        }
+        # Add more player(s) here
     },
+
+    # Define Tank colors here
+    #   - Any Tank with no color defined would be colored black
     "PLAYER_COLORS": {
         0: "blue",
         1: "red",
@@ -33,22 +47,68 @@ settings = {
         6: "brown",
         7: "cyan"
     },
+
+    # Size of game window
     "WINDOW_WIDTH": 1200,
     "WINDOW_HEIGHT": 1000,
+
+    # Size of each square MapCell
     "MAPCELL_SIZE": 100,
+
+    # Half the width of each Wall
+    #   - Each Wall is placed along the boundary between 2 MapCells
     "WALL_HALF_WIDTH": 5,
+
+    # Length (front to back) of each Tank
     "TANK_SIZE": 30,
+
+    # The ratio of each Tank's width to its length (as defined by "TANK_SIZE")
     "TANK_PROPORTION": 0.75,
+
+    # Radius of Projectiles
     "PROJECTILE_RADIUS": 4, # Default for Projectiles
+
+    # Radius for Bullets
     "BULLET_RADIUS": 4,
+
+    # Number of rows and columns of the Maze/Map
     "NUM_ROWS": 7,
     "NUM_COLS": 10,
-    "MARGIN": 100, # Adjust to center map
+
+    # Adjust this to ensure that the Map is drawn in the center of the window
+    "MARGIN": 100,
+
+    # Default speed of Tanks (number of pixels moved each frame)
     "TANK_SPEED": 3,
-    "MIN_STARTING_MANHATTAN_SEPARATION_RATIO": 0 , # E.g. If NUM_COLS = 10, Tanks' starting positions must be at least 0.7*10 = 7 cells apart (Manhattan distance)
+
+    # Important parameter in determining tank starting positions
+    #   - Describes the minimum Manhattan separation (rows between + cols
+    #     between) between any pair of 2 Tanks, as a ratio of the minimum
+    #     of the number of rows and number of cols
+    #   - E.g. If set to 0.4, and Map has 5 rows and 10 cols, game would
+    #     ensure that any pair of Tanks would be separated by at least
+    #     0.4 * 5 = 2 cells
+    #   - For a particular combination of number of Tanks and size of Map,
+    #     there is a maximum value for this parameter - if it is set to
+    #     anything greater, the game would take an extremely long time to
+    #     find accepted starting positions
+    #   - Use a high value (e.g. 0.5) for 2 Tanks
+    #   - Use 0 for larger numbers of Tanks, or to avoid crashing
+    "MIN_STARTING_MANHATTAN_SEPARATION_RATIO": 0,
+
+    # Bullets each Tank starts with (replenishes when Bullet despawns)
     "TANK_STARTING_AMMO": 5,
+
+    # Maximum number of Bullets each Tank can have
     "TANK_MAX_AMMO": 5,
+
+    # Number of frames each Projectile lasts before despawning
     "PROJECTILE_LIFETIME_FRAMES": 400,
+
+    # Quantization level of the Tank's direction angle
     "D_THETA": 5,
-    "AI_FIRE_DELAY": 10 # Number of frames of delay in AI's firing rate
+
+    # Number of frames of delay in AI's firing rate
+    #   - Without this setting, AI would fire once per frame, which is too fast
+    "AI_FIRE_DELAY": 10
 }
